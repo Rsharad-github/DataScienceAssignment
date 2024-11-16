960158f2762aaebec175cb1afd01539e912f687f
Rsharad-github <145047840+Rsharad-github@users.noreply.github.com>
2024-11-16 22:18:02 +0530

    Update and rename neuralNetwork.py to NeuralNetwork.py

--- NeuralNetwork.py
+++ NeuralNetwork.py
@@ -5,23 +5,24 @@ from sklearn.model_selection import train_test_split
 from sklearn.datasets import load_iris
 
 data = load_iris()
-# The dataset contains information about three types of iris flowers (setosa, versicolor, virginica) and their features:
-# Sepal length.
-# Sepal width.
-# Petal length.
-# Petal width.
+# The dataset contains information about three types of iris flowers (setosa, versicolor, virginica
+) and their features:
+# Sepal length. Sepal width.Petal length.Petal width.
 
 X = data.data#X will now contain the 4 feature values for each sample.i.e,sepal width
-y = data.target#y is a 1D array containing integers: 0, 1, and 2, representing the three flower species.
+y = data.target#y(array) has values 0, 1, and 2, representing the three flower species.
 
 # Train-test split
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=42)
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state = 42)
 #Using the same random_state guarantees that you get the same split every time.
 
 model = Sequential()
 model.add(Dense(10, input_shape=(X_train.shape[1],), activation='relu'))#represents the number of features in the training data.
 model.add(Dense(8, activation='relu'))#ReLU(activation fn) sets all negative values to 0 and keeps positive values unchanged.
 model.add(Dense(3, activation='softmax'))#Converts the output of the 3 neurons into probabilities.
+
+
+
 model.compile(optimizer='adam', loss='sparse_categorical_crossentropy', metrics=['accuracy'])
 #adam Automatically adjusts the learning rate for each parameter during training.
 #sparse_categorical_crossentropy is used to measure the difference between the predicted output and the actual target labels.
